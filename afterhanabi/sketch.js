6f5277f2921d24f813d552efcb315021d6386518
midroreee <starrynightyana@gmail.com>
2019-06-22 22:17:29 +0900

    test2

--- afterhanabi/sketch.js
+++ afterhanabi/sketch.js
@@ -1,12 +1,12 @@
 function setup() {
-  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> after hanabi");
   title.position(20, 0);
 
   canvas = createCanvas(400, 400);
   canvas.position(20, 60);
   canvas.class("artwork");
 
-  description = 작품에 대한 설명"\
+  description = "\
   작품에 대한 설명이 들어갑니다. <br/> \
   HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
   ";
